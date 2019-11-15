fd074550c449f80ad93875cbb046e83c7b3e9846
Antonio Lopes <antonio@alopes.co.uk>
2019-11-15 14:05:58 +0000

    zeit.co broken libpng example

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <img src={require("../public/cars.png?webp")} />
+    <img src={require("../public/cars.png")} />
+  </div>
+);
+
+export default Home;
